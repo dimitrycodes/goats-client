481105e960f008b10dc93a6010a188b70d8fa4d6
Dimitry A <strongsafetyking@yahoo.com>
2021-03-02 09:54:43 +0530

    Resolve the error in the react app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
-render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("#root")
-);
-//render(<App />, document.getElementById('root'));
+// render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+render(<App />, document.getElementById('root'));
